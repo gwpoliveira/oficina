153ad8beb244dcf03cf6d4507a50cc8a8bd90e4e
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-03-18 21:26:42 -0300

    login

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    template_name = 'usuarios/login.html'    
+    context  = {}
+    return render(request, template_name, context)
