2f786afa92ebfb20448540fb9be99695563cc1a7
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-04-03 20:40:47 -0300

    commit

--- apps/geral/views.py
+++ apps/geral/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contib.auth import login_required
+from django.contrib.auth import login_required
 
 
 from .forms import OficinaForm
