8d9b4fc6588ad316ab81d39bceabd4ec73f78a84
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-04-03 20:19:55 -0300

    viw

--- apps/geral/views.py
+++ apps/geral/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contib.auth import login_required
 
-# Create your views here.
+
+from .forms import OficinaForm
+from .models import Oficina
+
+@login_required
+def nova_oficina(request):
+    template_name = 'geral/nova_oficina.html'
+    context = {}
+    if request.method == 'POST':
+        form = OficinaForm(request.POST)
+        if form.is_valid():
+            of = form.save(commit=False)
+            of.usuario = request.user
+            of.save()
+            messages.sucesses(request, 'Oficina cadastradada com sucesso')
+            return redirect('geral:lista_oficina')
+    return render()
+
+
+def lista_oficina(request):
+    template_name = 'geral/lista_oficina.html'
+    oficinas = Oficina.objects.filter(usuario=request.user) # select * from oficina where usuario = usuario_de_sessão
+    context ={
+        'oficina': oficinas,
+    }
+    return render(request, template_name, context)
